04b90962be305b3db89f5fcab38ec9769f685584
Anastasia Gurevich <ngurevich23@mail.ru>
2017-01-18 15:49:46 +0400

    configure app testing

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ var express = require('express');
 var app = express();
 
 //View engine
-var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -16,13 +25,29 @@ app.set('port', process.env.PORT || 3000);
 //Static files
 app.use(express.static(__dirname + '/public'));
 
+//tests
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 //Routes
 app.get('/', function(req, res) {
     res.render('home', {motto: mottos.getMotto()});
 });
 
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', {
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/lesson-info', function(req, res) {
+    res.render('lesson-info');
+});
+
+app.get('/lesson-materials', function(req, res) {
+    res.render('lesson-materials');
 });
 
 //404
