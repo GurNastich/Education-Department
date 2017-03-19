2da44f0640159a6d012613315b8ad5d14c9bcc91
Anastasia Gurevich <ngurevich23@mail.ru>
2017-03-19 23:32:11 +0300

    add script to json file

---
+++ credentials.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mongo: {
+		development: {
+			connectionString: 'mongodb://Carl:588@ds159767.mlab.com:59767/edu'
+			// connectionString: 'mongodb://localhost:27017/test'	//local db
+		},
+		production: {
+			connectionString: 'mongodb://Carl:588@ds159767.mlab.com:59767/edu'
+			// connectionString: 'mongodb://localhost:27017/test'	//local db
+		}
+	}
+};
